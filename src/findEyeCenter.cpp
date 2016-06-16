a8723e068bd147e7fe34ce17f5b6d453082a50bd
Alojzij Blatnik <lojze.blatnik@gmail.com>
2016-06-16 21:59:32 +0200

    upd cleaning

--- src/findEyeCenter.cpp
+++ src/findEyeCenter.cpp
@@ -2,45 +2,13 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
-//#include <mgl2/mgl.h>
-
 #include <iostream>
 #include <queue>
 #include <stdio.h>
 
 #include "constants.h"
 #include "helpers.h"
-
-// Pre-declarations
-cv::Mat floodKillEdges(cv::Mat &mat);
-
-#pragma mark Visualization
-/*
-template<typename T> mglData *matToData(const cv::Mat &mat) {
-  mglData *data = new mglData(mat.cols,mat.rows);
-  for (int y = 0; y < mat.rows; ++y) {
-    const T *Mr = mat.ptr<T>(y);
-    for (int x = 0; x < mat.cols; ++x) {
-      data->Put(((mreal)Mr[x]),x,y);
-    }
-  }
-  return data;
-}
-
-void plotVecField(const cv::Mat &gradientX, const cv::Mat &gradientY, const cv::Mat &img) {
-  mglData *xData = matToData<double>(gradientX);
-  mglData *yData = matToData<double>(gradientY);
-  mglData *imgData = matToData<float>(img);
-  
-  mglGraph gr(0,gradientX.cols * 20, gradientY.rows * 20);
-  gr.Vect(*xData, *yData);
-  gr.Mesh(*imgData);
-  gr.WriteFrame("vecField.png");
-  
-  delete xData;
-  delete yData;
-  delete imgData;
-}*/
+#include "findEyeCenter.h"
 
 #pragma mark Helpers
 
@@ -107,7 +75,7 @@ cv::Point findEyeCenter(cv::Mat face, cv::Rect eye, std::string debugWindow) {
   cv::Mat eyeROI;
   scaleToFastSize(eyeROIUnscaled, eyeROI);
   // draw eye region
-  rectangle(face,eye,1234);
+  // rectangle(face,eye,1234);
   //-- Find the gradient
   cv::Mat gradientX = computeMatXGradient(eyeROI);
   cv::Mat gradientY = computeMatXGradient(eyeROI.t()).t();
@@ -134,7 +102,7 @@ cv::Point findEyeCenter(cv::Mat face, cv::Rect eye, std::string debugWindow) {
       }
     }
   }
-  imshow(debugWindow,gradientX);
+  // imshow(debugWindow,gradientX);
   //-- Create a blurred and inverted image for weighting
   cv::Mat weight;
   GaussianBlur( eyeROI, weight, cv::Size( kWeightBlurSize, kWeightBlurSize ), 0, 0 );
@@ -151,7 +119,7 @@ cv::Point findEyeCenter(cv::Mat face, cv::Rect eye, std::string debugWindow) {
   // Note: these loops are reversed from the way the paper does them
   // it evaluates every possible center for each gradient location instead of
   // every possible gradient location for every center.
-  printf("Eye Size: %ix%i\n",outSum.cols,outSum.rows);
+//  printf("Eye Size: %ix%i\n",outSum.cols,outSum.rows);
   for (int y = 0; y < weight.rows; ++y) {
     const double *Xr = gradientX.ptr<double>(y), *Yr = gradientY.ptr<double>(y);
     for (int x = 0; x < weight.cols; ++x) {
@@ -179,7 +147,7 @@ cv::Point findEyeCenter(cv::Mat face, cv::Rect eye, std::string debugWindow) {
     cv::threshold(out, floodClone, floodThresh, 0.0f, cv::THRESH_TOZERO);
     if(kPlotVectorField) {
       //plotVecField(gradientX, gradientY, floodClone);
-      imwrite("eyeFrame.png",eyeROIUnscaled);
+      // imwrite("eyeFrame.png",eyeROIUnscaled);
     }
     cv::Mat mask = floodKillEdges(floodClone);
     //imshow(debugWindow + " Mask",mask);
--- src/findEyeCenter.h
+++ src/findEyeCenter.h
@@ -4,5 +4,6 @@
 #include "opencv2/imgproc/imgproc.hpp"
 
 cv::Point findEyeCenter(cv::Mat face, cv::Rect eye, std::string debugWindow);
+cv::Mat floodKillEdges(cv::Mat &mat);
 
 #endif
